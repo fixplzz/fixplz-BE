08d10a5e888dcabfdecd8e23699f62578f1626a0
manje cho <whakswp123@naver.com>
2023-10-08 23:01:33 +0900

    setting: 초기 파일 세팅

---
+++ src/main/java/com/fixplz/FixplzApplication.java
@@ -0,0 +1,13 @@
+package com.fixplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixplzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixplzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fixplz/FixplzApplicationTests.java
@@ -0,0 +1,13 @@
+package fixplz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixplzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
