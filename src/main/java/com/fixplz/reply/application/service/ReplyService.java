e89316d33e3b4763866e01c80cf5545af6047cca
numerical43 <numerical43@daum.net>
2023-10-19 22:39:13 +0900

    feature : #3 답변 entity, 기본 클래스 생성

---
+++ src/main/java/com/fixplz/reply/application/service/ReplyService.java
@@ -0,0 +1,9 @@
+package com.fixplz.reply.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+}
---
+++ src/main/java/com/fixplz/reply/domain/aggregate/entity/Reply.java
@@ -0,0 +1,41 @@
+package com.fixplz.reply.domain.aggregate.entity;
+
+import com.fixplz.reply.domain.aggregate.vo.PostNoVO;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Table(name = "TBL_REPLY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyNo;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private Date date;
+
+    @Embedded
+    private PostNoVO postNoVO;
+
+    @Builder
+    public Reply(String title, String content, Date date, PostNoVO postNoVO) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.postNoVO = postNoVO;
+    }
+}
