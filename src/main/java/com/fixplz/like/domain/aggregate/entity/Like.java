02595efa0446d2a1c18938882d29f94b3098d852
numerical43 <numerical43@daum.net>
2023-10-24 17:33:26 +0900

    bugfix : Like의 테이블명을 TBL_LIKE로 수정

--- src/main/java/com/fixplz/like/domain/aggregate/entity/Like.java
+++ src/main/java/com/fixplz/like/domain/aggregate/entity/Like.java
@@ -3,6 +3,7 @@ package com.fixplz.like.domain.aggregate.entity;
 import com.fixplz.like.domain.aggregate.vo.LikeVO;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TBL_LIKE")
 public class Like {
 
     @EmbeddedId
