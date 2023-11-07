35c9716afb0d3a207bcdff571a6dab7d52fc8bd1
SonJungin <jungin.dylan@gmail.com>
2023-11-07 11:26:39 +0900

    feat: #31 config 설정

---
+++ src/main/java/com/fixplz/configuration/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.fixplz.configuration;
+
+import com.fixplz.auth.JwtTokenProvider;
+import com.fixplz.auth.Jwt401Handler;
+import com.fixplz.auth.Jwt403Handler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final Jwt401Handler jwt401Handler;
+    private final Jwt403Handler jwt403Handler;
+
+    private final String[] API_WHITE_LIST = {
+            "/admin/regist",
+            "/admin/login"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwt401Handler)
+                .accessDeniedHandler(jwt403Handler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(API_WHITE_LIST).permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(jwtTokenProvider));
+
+        return http.build();
+    }
+
+}
