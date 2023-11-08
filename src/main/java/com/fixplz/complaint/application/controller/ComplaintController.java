95133f0d1f21a1bc6af78363684db0701ad6c3ee
numerical43 <numerical43@daum.net>
2023-11-08 19:34:26 +0900

    feature : controller 미완성 코드 삭제

--- src/main/java/com/fixplz/complaint/application/controller/ComplaintController.java
+++ src/main/java/com/fixplz/complaint/application/controller/ComplaintController.java
@@ -1,9 +1,6 @@
 package com.fixplz.complaint.application.controller;
 
-import com.fixplz.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/vi/complaint")
 public class ComplaintController {
 
-    @PostMapping
-    public ResponseEntity<ApiResponse> createComplaint() {
-
-    }
 }
